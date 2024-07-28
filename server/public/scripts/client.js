165928be210fa2f7558e9e4bd958f5dd410cad46
Elwood Nelson <elwoodnelson@gmail.com>
2024-07-28 15:45:53 -0500

    post and delete are both working, all tests passed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -20,36 +20,107 @@ function getTodoList () {
 
 // Make POST function to add a job
 
+function addTodo(event){
+    event.preventDefault();
+    const makeTodo = {
+        toDo: document.querySelector('#enterTask').value
+        }
+    console.log('sending new todo')
+    axios({
+        method: 'POST',
+        url: '/todos',
+        data: makeTodo
+      }).then(function(response) {
+        console.log(response.data);
+        document.getElementById('entry').reset();
+        getTodoList();
+    
+      }).catch(function(error) {
+        console.log('error in todo POST', error); 
+        alert('Error adding to-do. Please try again later.')       
+      });
+    
+ }
+
+
 
 
 
 // Make PUT function to update job status
 
-
+function jobComplete(finishId) {
+    const jobId = finishId;
+    console.log('the job id is',jobId)
+    axios ({
+        method: "PUT",
+        url: (`/todos/${jobId}`)
+    }) 
+    .then((response) => {
+        console.log('changed job status')
+        getTodoList();
+    })
+    .catch((error) => {
+        console.log('Error in changing job status', error)
+    })
+}
 
 
 // Make DELETE function to remove job
-
+function removeJob(taskId) {
+    const jobId = taskId
+    // Swal.fire({
+    //     title: "Are you sure you want to delete?",
+    //     showDenyButton: true,
+    //     showCancelButton: false,
+    //     confirmButtonText: "Yes, Delete",
+    //     denyButtonText: `No, Do Not Delete`
+    //   }).then((result) => {
+    //     if (result.isConfirmed) {
+    //       Swal.fire("Deleted", "", "success");
+        axios({
+            method: 'DELETE',
+            url: `/todos/${jobId}`
+        })
+        .then((response) => {
+            getTodoList();
+            console.log('job has been deleted')
+        })
+        .catch(error => {
+            console.log('there was an error deleting the job', error)
+        })
+//         }  // end if
+//         else if (result.isDenied) {
+//             Swal.fire("No Changes Made", "", "success");
+//         }
+//     });
+}    
 
 // Make function to DISPLAY the job list
 function displayList(jobs){
-    let complete = 'Unfinished';
-    console.log(jobs);
+
+    document.querySelector('.jobList').innerHTML = ``
 
     let tableSpot = document.querySelector('.jobList')
     for (let item of jobs){
+        let complete = 'Unfinished';
         if(item.isComplete === true){
             complete = 'Done'
+            tableSpot.innerHTML += `
+            <tr data-testid="toDoItem" class="completed">
+            <td>${item.text}</td>
+            <td>${complete}</td>
+            <td><button data-testid="completeButton" onClick="jobComplete(${item.id})">Mark Unfinished</button>
+            <td><button data-testid="deleteButton" onClick="removeJob(${item.id})">Delete</button>
+            `
         }
-        else{complete = "Unfinished"}
-        console.log(complete)
+        else{complete = "Unfinished"
         tableSpot.innerHTML += `
-        <tr data-testid="toDoItem class="incomplete">
+        <tr data-testid="toDoItem" class="incomplete">
         <td>${item.text}</td>
         <td>${complete}</td>
         <td><button data-testid="completeButton" onClick="jobComplete(${item.id})">Mark Complete</button>
         <td><button data-testid="deleteButton" onClick="removeJob(${item.id})">Delete</button>
         `
-
+        }
     }
 }
